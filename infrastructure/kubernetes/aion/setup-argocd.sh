94f55e25fabe5cf9e103954bb4bf9eafdc973f53
Vince <vincivenv@gmail.com>
2025-12-14 21:48:29 +0100

    template controlplane yaml and add the private repo as source too

--- infrastructure/kubernetes/aion/setup-argocd.sh
+++ infrastructure/kubernetes/aion/setup-argocd.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "Waiting for ArgoCD namespace to exist..."
+echo "waiting for argocd namespace to exist..."
 for i in {1..12}; do
   if kubectl get namespace argocd >/dev/null 2>&1; then
-    echo "ArgoCD namespace found!"
+    echo "argocd namespace found"
     break
   fi
   if [ $i -eq 12 ]; then
-    echo "Timed out waiting for ArgoCD namespace"
+    echo "timed out waiting for argocd namespace"
     exit 1
   fi
-  echo "ArgoCD namespace not found yet. Waiting 5 seconds..."
+  echo "argocd namespace not found yet, waiting 5 seconds..."
   sleep 5
 done
 
-echo "Creating repository secret with SSH key..."
+echo "creating repository secrets with ssh key..."
+
 kubectl -n argocd apply -f - <<EOF
 apiVersion: v1
 kind: Secret
@@ -31,4 +32,24 @@ $(cat ${HOME}/.ssh/id_ed25519 | sed 's/^/    /')
   insecure: "true"
   enableLfs: "false"
 EOF
-echo "Added GitHub repository with SSH authentication!"
+echo "added homelab repository"
+
+kubectl -n argocd apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: repo-github-hllab
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: repository
+stringData:
+  type: git
+  url: git@github.com:mrdvince/hllab.git
+  sshPrivateKey: |
+$(cat ${HOME}/.ssh/id_ed25519 | sed 's/^/    /')
+  insecure: "true"
+  enableLfs: "false"
+EOF
+echo "added hllab repository"
+
+echo "github repositories configured with ssh authentication"
